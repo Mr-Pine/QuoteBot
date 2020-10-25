7eb4f5ac50e3236143a66e447bf61d7e925bf782
Mr. Pine <kieferdavid@outlook.com>
2020-10-25 22:47:03 +0100

    sending of serverList

--- src/serverQuoteListener.ts
+++ src/serverQuoteListener.ts
@@ -7,29 +7,39 @@ export function listenServer(client: Discord.Client) {
 
     const server = http.createServer(function (request, response) {
 
-        var body = ''
-        request.on('data', function (data) {
-            body += data
-        })
-        request.on('end', function () {
-            body = decodeURIComponent(body)
-            body = body.split("%20").join(" ")
-            console.log('Body: ' + body)
-            var bodyJSON = getBodyJSON(body)
-            console.log(`content: ${bodyJSON["quote"].toString()}`)
-            console.log(`server: ${bodyJSON["server"]}`)
-            console.log(`user: ${bodyJSON["user"]}`)
-            response.writeHead(200, { 'Content-Type': 'text/plain' })
-            response.end('post received')
+        var settings = JSON.parse(readFileSync("./src/settings.json").toString())
 
-            var settings = JSON.parse(readFileSync("./src/settings.json").toString())
+        switch (request.url) {
+            case '/server-list': {
+                response.writeHead(200, { 'Content-Type': 'application/json' })
+                response.end(JSON.stringify({servers: Object.keys(settings)}))
+            }
+            case '/': {
+                var body = ''
+                request.on('data', function (data) {
+                    body += data
+                })
+                request.on('end', function () {
+                    body = decodeURIComponent(body)
+                    body = body.split("%20").join(" ")
+                    console.log('Body: ' + body)
+                    var bodyJSON = getBodyJSON(body)
+                    console.log(`content: ${bodyJSON["quote"].toString()}`)
+                    console.log(`server: ${bodyJSON["server"]}`)
+                    console.log(`user: ${bodyJSON["user"]}`)
+                    response.writeHead(200, { 'Content-Type': 'text/plain' })
+                    response.end('post received')
 
 
-            client.channels.fetch(settings[bodyJSON["server"]]["QUOTE_CHANNEL_ID"] as string).then(quoteChannel => {
-                createQuote(quoteChannel as Discord.TextChannel, bodyJSON["quote"].toString(), bodyJSON["user"], bodyJSON["server"])
-            })
 
-        })
+                    client.channels.fetch(settings[bodyJSON["server"]]["QUOTE_CHANNEL_ID"] as string).then(quoteChannel => {
+                        createQuote(quoteChannel as Discord.TextChannel, bodyJSON["quote"].toString(), bodyJSON["user"], bodyJSON["server"])
+                    })
+
+                })
+            }
+        }
+
     })
 
     const port = 3000
