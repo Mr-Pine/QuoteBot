710ace7d333f0c171a28f5fe2be5aa464aa977dd
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2021-03-09 16:31:56 +0100

    Implemented Express (#5)
    * Bump ini from 1.3.5 to 1.3.8
    
    Bumps [ini](https://github.com/isaacs/ini) from 1.3.5 to 1.3.8.
    - [Release notes](https://github.com/isaacs/ini/releases)
    - [Commits](https://github.com/isaacs/ini/compare/v1.3.5...v1.3.8)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * added package-lock.json to gitignore
    
    * package-lock.json
    
    * deleted package-lock.json
    
    * switched to express
    
    * Update README.md
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Mr. Pine <50425705+Mr-Pine@users.noreply.github.com>
    :)

--- src/serverQuoteListener.ts
+++ src/serverQuoteListener.ts
@@ -1,104 +1,114 @@
-import * as http from "http"
+// import * as http from "http"
 import { createQuote } from "./quoteListener"
 import { readFileSync } from "fs"
 import * as Discord from "discord.js"
 import { removeQuote } from './commands/remove'
 import { editQuote } from "./commands/edit"
+const express = require('express');
 
 export function listenServer(client: Discord.Client) {
 
-    const server = http.createServer(function (request, response) {
-
-        var settings = JSON.parse(readFileSync("./src/settings.json").toString())
-
-        switch (request.url) {
-            case '/server-list': {
-                response.setHeader('Content-Type', 'text/plain')
-                response.setHeader("Access-Control-Allow-Origin", "*");
-                //response.statusCode = 200
-                response.end(JSON.stringify({ servers: Object.keys(settings) }))
-                break;
-            }
-            case '/': {
-                var body = ''
-                request.on('data', function (data) {
-                    body += data
-                })
-                request.on('end', function () {
-                    body = decodeURIComponent(body)
-                    body = body.split("%20").join(" ")
-                    console.log('Body: ' + body)
-                    var bodyJSON = getBodyJSON(body)
-                    console.log(`content: ${bodyJSON["quote"].toString()}`)
-                    console.log(`server: ${bodyJSON["server"]}`)
-                    console.log(`user: ${bodyJSON["user"]}`)
-                    response.writeHead(200, { 'Content-Type': 'text/plain' })
-                    response.end('post received')
-
-
-
-                    client.channels.fetch(settings[bodyJSON["server"]]["QUOTE_CHANNEL_ID"] as string).then(quoteChannel => {
-                        createQuote(quoteChannel as Discord.TextChannel, bodyJSON["quote"].toString(), bodyJSON["user"], bodyJSON["server"])
-                    })
-
-                })
-
-                return
-            }
-            case '/delete': {
-                console.log("delete")
-
-                var body = ''
-                request.on('data', function (data) {
-                    body += data
-                })
-                request.on('end', function () {
-                    body = decodeURIComponent(body)
-                    body = body.split("%20").join(" ")
-                    var bodyJSON = getBodyJSON(body)
-                    response.writeHead(200, { 'Content-Type': 'text/plain' })
-                    response.end('post received')
-
-                    console.log(bodyJSON)
-
-                    var member = client.guilds.cache.get(bodyJSON['server'])?.members.cache.get(bodyJSON['user'])
-                    console.log(member)
-
-                    removeQuote(bodyJSON['number'], bodyJSON['server'], client, member as Discord.GuildMember)
-                })
-                return
-            }
-            case '/edit': {
-                console.log("edit")
-
-                var body = ''
-                request.on('data', function (data) {
-                    body += data
-                })
-                request.on('end', function () {
-                    body = decodeURIComponent(body)
-                    body = body.split("%20").join(" ")
-                    var bodyJSON = getBodyJSON(body)
-                    response.writeHead(200, { 'Content-Type': 'text/plain' })
-                    response.end('post received')
-
-                    
-                    var member = client.guilds.cache.get(bodyJSON['server'])?.members.cache.get(bodyJSON['user'])
-
-                    editQuote(bodyJSON["number"], bodyJSON["quote"].toString(), bodyJSON["server"], client, member as Discord.GuildMember)
-
-
-                })
-
-                response.end("received")
-                return
-            }
-        }
-
-    })
+    const settings = JSON.parse(readFileSync("./src/settings.json").toString())
 
     const port = 3000
     const host = 'localhost'//'192.168.178.52'
+
+    const apiURL = "/api";
+
+    const app = express();
+    const server = app.listen(port, () => {
+        console.log(`Express running → PORT ${server.address().port}`);
+    });
+
+
+    app.get('/server-list', (req: any, res: any) => {
+
+
+        res.setHeader('Content-Type', 'text/plain')
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        // res.statusCode = 200
+        res.send(JSON.stringify({ servers: Object.keys(settings) }))
+
+    })
+
+    app.get('/', (req: any, res: any) => {
+
+        res.sendStatus(200);
+
+    })
+
+
+    app.post(apiURL, async (req: any, res: any) => {
+
+        var body = ''
+        req.on('data', function (data: any) {
+            body += data
+        })
+        req.on('end', function () {
+            body = decodeURIComponent(body)
+            body = body.split("%20").join(" ")
+            console.log('Body: ' + body)
+            var bodyJSON = getBodyJSON(body)
+            console.log(`content: ${bodyJSON["quote"].toString()}`)
+            console.log(`server: ${bodyJSON["server"]}`)
+            console.log(`user: ${bodyJSON["user"]}`)
+            res.sendStatus(200);
+
+
+            client.channels.fetch(settings[bodyJSON["server"]]["QUOTE_CHANNEL_ID"] as string).then(quoteChannel => {
+                createQuote(quoteChannel as Discord.TextChannel, bodyJSON["quote"].toString(), bodyJSON["user"], bodyJSON["server"])
+            })
+
+        })
+
+    });
+
+    app.delete(apiURL, async (req: any, res: any) => {
+        console.log("delete")
+
+        var body = ''
+        req.on('data', function (data: any) {
+            body += data
+        })
+        req.on('end', function () {
+            body = decodeURIComponent(body)
+            body = body.split("%20").join(" ")
+            var bodyJSON = getBodyJSON(body)
+            req.sendStatus(200);
+
+            console.log(bodyJSON)
+
+            var member = client.guilds.cache.get(bodyJSON['server'])?.members.cache.get(bodyJSON['user'])
+            console.log(member)
+
+            removeQuote(bodyJSON['number'], bodyJSON['server'], client, member as Discord.GuildMember)
+        })
+    });
+
+    app.patch(apiURL, async (req: any, res: any) => {
+
+        console.log("edit")
+
+        var body = ''
+        req.on('data', function (data: any) {
+            body += data
+        })
+        req.on('end', function () {
+            body = decodeURIComponent(body)
+            body = body.split("%20").join(" ")
+            var bodyJSON = getBodyJSON(body)
+            res.sendStatus(200);
+
+
+            var member = client.guilds.cache.get(bodyJSON['server'])?.members.cache.get(bodyJSON['user'])
+
+            editQuote(bodyJSON["number"], bodyJSON["quote"].toString(), bodyJSON["server"], client, member as Discord.GuildMember)
+
+
+        })
+
+    })
+
     server.listen(port, host)
     console.log(`Listening at http://${host}:${port}`)
 
